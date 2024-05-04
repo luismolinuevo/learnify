657e5b50c2ebc6922938a76b9b8de9c0bf8f5f9e
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-04 11:11:47 -0400

    create learning plan api call

--- client/src/lib/learningplan.js
+++ client/src/lib/learningplan.js
@@ -21,4 +21,39 @@ export const getLearningPlan = async (planId) => {
       return error;
     }
   };
+
+  export const createLearningPlan = async (data) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_PUBLIC_BACKEND_URL}/api/learningplan`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            //   Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        }
+      );
+  
+      if (!response.ok) {
+        console.error(
+          "Create learning plan request request failed. Response status:",
+          response.status
+        );
+        const errorData = await response.json();
+        console.error("Error details:", errorData);
+  
+        throw new Error("Create learning plan failed");
+      }
+  
+      // If the response is successful, return the data (if needed)
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      // Log more details about the error
+      console.error("Error during creating learning plan:", error);
+      throw error;
+    }
+  };
   

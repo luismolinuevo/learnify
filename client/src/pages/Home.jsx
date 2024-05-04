7d8d64204ff0510eedaa5946e5bab7e2e02788cf
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-04 13:06:11 -0400

    some more styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import LearningPlanCard from "../components/general/LearningPlanCard";
 import "../styles/Home.css";
 import EastIcon from "@mui/icons-material/East";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -16,10 +17,10 @@ export default function Home() {
           <LearningPlanCard />
           <LearningPlanCard />
         </div>
-        <button className="more-button add-plan">
+        <Link to={`/auth/signup/learn`} className="more-button add-plan">
           <AddCircleIcon className="icons add-plan-icon" fontSize="" />
           <p>Add a plan</p>
-        </button>
+        </Link>
         <div className="more-button-container">
           <button className="more-button">
             <p>More</p>

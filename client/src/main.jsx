536d5989d7f011786d36b952d6d4d9e0fb133064
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-04 04:36:18 -0400

    fixed course page rendering

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,66 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './styles/App.css';
+import "./styles/App.css";
 //Add your imports from "/pages" below this line
-import AuthLayout from './pages/AuthLayout.jsx';
-import Learn from './components/signupFields/learn.jsx';
-import Level from './components/signupFields/level.jsx';
-import Daily from './components/signupFields/daily.jsx';
-import Success from './components/signupFields/success.jsx';
+import AuthLayout from "./pages/AuthLayout.jsx";
+import Learn from "./components/signupFields/learn.jsx";
+import Level from "./components/signupFields/level.jsx";
+import Daily from "./components/signupFields/daily.jsx";
+import Success from "./components/signupFields/success.jsx";
 import Layout from "./pages/Layout.jsx";
 
 import Course from "./pages/Course.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/auth",
-        element: <AuthLayout />,
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: "",
+      },
+      {
+        path: "signup",
         children: [
-            {
-                path: "login",
-                element: "",
-            },
-            {
-                path: "signup",
-                children: [
-                    {
-                        path: "learn",
-                        element: <Learn />,
-                    },
-                    {
-                        path: "level",
-                        element: <Level />,
-                    },
-                    {
-                        path: "daily",
-                        element: <Daily />,
-                    },
-                    {
-                        path: "success",
-                        element: <Success />,
-                    },
-                ],
-            },
+          {
+            path: "learn",
+            element: <Learn />,
+          },
+          {
+            path: "level",
+            element: <Level />,
+          },
+          {
+            path: "daily",
+            element: <Daily />,
+          },
+          {
+            path: "success",
+            element: <Success />,
+          },
         ],
-    },
-    {
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
         path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: ""
-            },
-            {
-                path: "lessonplan/:id",
-            }
-        ]
-    },
+        element: "",
+      },
+      {
+        path: "lessonplan/:id",
+        element: <Course />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );

2ca18f288556ada6d03e82feb7d23f2934cd800b
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-03 22:07:06 -0400

    got learning plan data in course page

--- client/src/pages/Course.jsx
+++ client/src/pages/Course.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LearningPlanCard from "../components/general/LearningPlanCard";
+import { getLearningPlan } from "../lib/learningplan";
+import { useParams } from "react-router-dom";
 
 export default function Course() {
+  let { id } = useParams();
+  const [learningPlan, setLearningPlan] = useState([]);
+
+  useEffect(() => {
+    const fetchLearningPlan = async () => {
+      try {
+        if (id) {
+          const fetch = await getLearningPlan(id);
+
+          if (fetch.success) {
+            console.log(fetch)
+            setLearningPlan(fetch.learningplan);
+          }
+        }
+      } catch (error) {
+        console.error("There has been a error fetching the lesson plan");
+      }
+    };
+
+    fetchLearningPlan();
+  }, [id]);
+  
   return (
     <div>
-      <LearningPlanCard/>
+      <LearningPlanCard learningPlan={learningPlan}/>
     </div>
   );
 }

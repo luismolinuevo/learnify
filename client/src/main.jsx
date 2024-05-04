382e73befd5ced6e983d21485fb84a9b1642ee4f
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-03 20:58:35 -0400

    created learning plan card

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,49 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 //Add your imports from "/pages" below this line
-import AuthLayout from './pages/AuthLayout.jsx';
-
-
+import AuthLayout from "./pages/AuthLayout.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/auth",
-        element: <AuthLayout />,
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: "",
+      },
+      {
+        path: "signup",
         children: [
-            {
-                path: "login",
-                element: "",
-            },
-            {
-                path: "signup",
-                children: [
-                    {
-                        path: "learn",
-                        element: "",
-                    },
-                    {
-                        path: "level",
-                        element: "",
-                    },
-                    {
-                        path: "daily",
-                        element: "",
-                    },
-                    {
-                        path: "success",
-                        element: "",
-                    },
-                ],
-            },
+          {
+            path: "learn",
+            element: "",
+          },
+          {
+            path: "level",
+            element: "",
+          },
+          {
+            path: "daily",
+            element: "",
+          },
+          {
+            path: "success",
+            element: "",
+          },
         ],
-    },
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+        {
+            path: "/",
+            element: ""
+        },
+        {
+            path: "lessonplan/:id",
+        }
+    ]
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
